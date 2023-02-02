d0c8e2368acf8d7302ca5921223eb788b7087d4a
Михайлов Дмитрий Юрьевич (4054833) <dumihailov@dev.vtb.ru>
2023-02-02 08:36:45 +0300

    edited build.gradle added SelenideTest

---
+++ src/test/java/study/qa/SelenideTest.java
@@ -0,0 +1,22 @@
+package study.qa;
+
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.By.*;
+
+public class SelenideTest {
+
+    @Test
+    public void testIssueSearch() {
+        open("https://github.com");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("#issues-tab").click();
+        $(withText("#80")).should(exist);
+    }
+}
